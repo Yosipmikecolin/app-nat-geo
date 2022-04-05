e851a68164eca473b45d8920179dd2d75a95167e
Yosip Mike Colin <colinparrado@gmail.com>
2022-04-05 17:49:30 -0500

    Eliminando rutas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Noticias from "./Views/Noticias";
-import Formulario from "./Views/Formulario";
-import Articulos from "./Views/Articulos";
+
 
 
 
@@ -14,17 +11,7 @@ ReactDOM.render(
 
 
   <React.StrictMode>
-    
-    <BrowserRouter>
-    
-
-    <Routes>
-      <Route path='/' element={<App />}/>
-      <Route path='/noticias' element={<Noticias/>}/>
-      <Route path='/sign' element={<Formulario/>}/>
-      <Route path="/noticias/:titulo" element={<Articulos/>}/>
-    </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
